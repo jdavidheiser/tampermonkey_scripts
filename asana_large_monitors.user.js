3bbf62227da20e8ad1d1b6106664128d29c343f7
James Davidheiser <jamesdavidheiser@asana.com>
2016-09-22 16:14:48 -0700

    initial commit

---
+++ asana_large_monitors.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         Asana for large monitors
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  maximize screen real estate when using Asana.
+// @author       JD
+// @match        https://app.asana.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function addGlobalStyle(css) {
+        var head, style;
+        head = document.getElementsByTagName('head')[0];
+        if (!head) { return; }
+        style = document.createElement('style');
+        style.type = 'text/css';
+        style.innerHTML = css;
+        head.appendChild(style);
+    }
+    addGlobalStyle('#right_pane_container { max-width: 1500px !important; width: 95% !important; }');
+})();
